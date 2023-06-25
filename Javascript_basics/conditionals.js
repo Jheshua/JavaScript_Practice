c9fb119304878f89471af02716b96471d5de1bef
Jheshua user <jheshua.jaramillo@hotmail.com>
2023-06-25 13:44:10 -0500

    second commit

--- Javascript_basics/conditionals.js
+++ Javascript_basics/conditionals.js
@@ -24,3 +24,71 @@ while(z < names1.length){
     console.log(names1[z]);
     z++;
 }
+
+// Do while loop
+let k = 0; 
+do{
+    console.log('Value: ' ,k);
+    k++;
+} while ( k < 5);
+
+
+// if statements
+
+const kok = 21;
+if(kok > 20 ){
+    console.log('You are over 20 years old');
+}
+
+const ninjas = ['shaun', 'ryu','jhesha','chun-li']; 
+if (ninjas.length > 3 ){
+    console.log('that is a lot of ninjas');
+}
+
+const password = 'passd';
+
+if(password.length >= 12){
+    console.log('this password is might strong');
+}
+else if(password.length >= 8){
+    console.log('that password is long ennough');
+} else {
+    console.log('The password is not long enough');
+}
+
+//logical NOT (!) negates the truth value of a boolean expression, resulting in the opposite boolean value.
+
+let user = false;
+
+if(!user){
+    console.log('You must be loged-in to continue');
+} 
+
+//Example of switch case 
+
+const numkl = 'A';
+
+switch (numkl) {
+  case 'A':
+    console.log('El valor es A');
+    break;
+  case 'B':
+    console.log('El valor es B');
+    break;
+  case 'C':
+    console.log('El valor es C');
+    break;
+  case 'D':
+    console.log('El valor es D');
+    break;
+  default:
+    console.log('El valor no coincide con ninguna opción');
+}
+
+
+
+
+
+
+
+
